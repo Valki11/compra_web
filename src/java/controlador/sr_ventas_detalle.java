a9c6416ac96b9f537d81ad9e410baca2936a32b4
Valki11 <102394314+Valki11@users.noreply.github.com>
2024-11-03 05:52:21 -0600

    .

--- src/java/controlador/sr_ventas_detalle.java
+++ src/java/controlador/sr_ventas_detalle.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package controlador;
 
 import java.io.IOException;
@@ -17,15 +14,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class sr_ventas_detalle extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
